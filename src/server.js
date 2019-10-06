07fa8ff458e1c87aae9666e8f801134107c56ac6
marcelovqvd <marcelovqvd@gmail.com>
2019-10-06 15:56:24 -0300

    Estrutura da aplicação

--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.get('/', (req, res) => {
-  return res.json({
-    message: "Hello world'});
-  });
-
-  app.listen(3333);
+app.listen(3333);
